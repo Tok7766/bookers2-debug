ac96b4ac077c652d0711812335fe162d82a05795
EC2 Default User <ec2-user@ip-172-31-6-217.ap-northeast-1.compute.internal>
2022-07-03 19:07:20 +0000

    いいね機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,11 @@
 class BooksController < ApplicationController
 
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def show
     @books = Book.new
     @book = Book.find(params[:id])
+    @book_comment = BookComment.new
   end
 
   def index
@@ -46,9 +49,10 @@ class BooksController < ApplicationController
     params.require(:book).permit(:title, :body)
   end
 
-  def correct_user
+  def ensure_correct_user
     @book = Book.find(params[:id])
-    @user = @book.user
-    redirect_to(books_path) unless @user == current_user
+   unless  @book.user == current_user
+    redirect_to books_path
+   end
   end
 end
